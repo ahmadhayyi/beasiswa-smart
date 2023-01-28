f8aa3d67fbe177824746f0f0e9624064cd9e0558
ahmadhayyi <ahmadhayyi33@gmail.com>
2023-01-28 15:40:40 +0700

    Mengganti label

--- resources/views/dashboard/bobot/create.blade.php
+++ resources/views/dashboard/bobot/create.blade.php
@@ -1,12 +1,12 @@
 @extends('dashboard.layout.main')
 
 @section('title')
-    <title>Tambah Bobot</title>
+    <title>Tambah Kriteria</title>
 @endsection
 
 @section('heading')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Tambah Bobot</h1>
+    <h1 class="h2">Tambah Kriteria</h1>
 </div>
 @endsection
 
--- resources/views/dashboard/bobot/edit.blade.php
+++ resources/views/dashboard/bobot/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('dashboard.layout.main')
 
 @section('title')
-    <title>Edit Bobot</title>
+    <title>Edit Kriteria</title>
 @endsection
 
 @section('heading')
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Edit Bobot</h1>
+    <h1 class="h2">Edit Kriteria</h1>
 </div>
 @endsection
 
